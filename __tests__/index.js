e1d4c62c6684af822984d7121098e060b5533201
jabher <crbrus@gmail.com>
2015-11-02 18:31:35 +0300

    bug: request bundling not working feat: network usage optimized

--- __tests__/index.js
+++ __tests__/index.js
@@ -8,25 +8,31 @@ jest.dontMock('../lib/util.js')
 
 const fetch = jest.genMockFn()
 jest.setMock('isomorphic-fetch', fetch)
-const getLastFetchCall = () => fetch.mock.calls[fetch.mock.calls.length - 1]
 
 const {VK} = require('../lib/index.js')
 const {Query} = require('../lib/QueryRunner.js')
 
+const jsonContents = JSON.stringify({v: Query.v})
+const app_id = 12345
+
+const getLastFetchCall = () => fetch.mock.calls[fetch.mock.calls.length - 1]
+const expectLastFetchCall = () => expect(getLastFetchCall())
+const mockResponseWithJson = (response = '') => {
+    const json = jest.genMockFn()
+    fetch.mockReturnValue(Promise.resolve({json}))
+    json.mockReturnValue(Promise.resolve({response}))
+}
+
 describe('VK Api', () => {
-    const app_id = 12345
     describe('unauthorized instance', () => {
         const vk = new VK(app_id)
         it('should perform non-authenticated call to VK', () => {
             vk.call('foo', {bar: 'baz'})
             jest.runAllTimers()
-            expect(getLastFetchCall()[0]).toEqual('https://api.vk.com/method/foo?bar=baz&v=' + Query.v)
+            expectLastFetchCall().toEqual('https://api.vk.com/method/foo?bar=baz&v=' + Query.v)
         })
         it('should pass the response', () => {
-            const json = jest.genMockFn()
-            fetch.mockReturnValue(Promise.resolve({json}))
-            json.mockReturnValue(Promise.resolve({response: {a: 'b'}}))
-
+            mockResponseWithJson({a: 'b'})
             vk.call('')
                 .then(result => expect(result.a).toEqual('b'))
             jest.runAllTicks()
@@ -34,26 +40,63 @@ describe('VK Api', () => {
         })
     })
     describe('authorized instance', () => {
+        it('should perform authenticated call to VK', () => {
+            const sid = app_id
+            Object.assign(new VK(app_id), {sid}).call('')
+            jest.runAllTimers()
+            expect(getLastFetchCall()[0]).toEqual('https://api.vk.com/method/?access_token=' + sid + '&v=' + Query.v)
+        })
+    })
+    describe('bunched calls', () => {
         const vk = new VK(app_id)
         vk.sid = app_id
-        it('should perform authenticated call to VK', () => {
-            vk.call('', {})
-            jest.runAllTimers()
-            expect(getLastFetchCall()[0]).toEqual('https://api.vk.com/method/?access_token=' + app_id + '&v=' + Query.v)
-        })
+        const checkExecuteCall = (queryString) =>
+            expect(getLastFetchCall()[0])
+                .toEqual('https://api.vk.com/method/execute?' +
+                    'access_token=' + app_id +
+                    '&code=' +
+                    encodeURIComponent('var results=[],result;' + queryString + 'return results;') +
+                    '&v=' + Query.v)
         it('should merge multiple requests', () => {
             vk.call('1', {})
             vk.call('2', {})
             const jsonContents = JSON.stringify({v: Query.v})
             jest.runAllTimers()
-            expect(getLastFetchCall()[0]).toEqual('https://api.vk.com/method/execute?' +
-                'access_token=' + app_id +
-                '&code=' +
-                encodeURIComponent('var results=[],result;' +
-                    'results.push(API.1('+jsonContents+'));' +
-                    'results.push(API.2('+jsonContents+'));' +
-                    'return results;') +
-                '&v=' + Query.v)
+            checkExecuteCall(
+                'results.push(API.1(' + jsonContents + '));' +
+                'results.push(API.2(' + jsonContents + '));')
+        })
+        it('should merge multiple requests with simple postfixes', () => {
+            vk.call('1', {}, '.1')
+            vk.call('2', {}, '.2')
+            jest.runAllTimers()
+            checkExecuteCall(
+                'results.push(API.1(' + jsonContents + ').1);' +
+                'results.push(API.2(' + jsonContents + ').2);')
+        })
+        it('should merge multiple requests with postfixes including ";" in a complex manner', () => {
+            vk.call('1', {}, '; result = 1')
+            vk.call('2', {}, '; result = 2')
+            jest.runAllTimers()
+            checkExecuteCall(
+                'result = API.1(' + jsonContents + '); result = 1;' +
+                'results.push(result);' +
+                'result = API.2(' + jsonContents + '); result = 2;' +
+                'results.push(result);')
+        })
+        it('should take a break if have more than 25 reqs', () => {
+            let i = 0
+            while (i++ < 45) vk.call('')
+            const response = []
+            while (response.length < 25)
+                response.push('')
+            const json = jest.genMockFn()
+            fetch.mockReturnValue(Promise.resolve({json}))
+            json.mockReturnValue(Promise.resolve({response}))
+
+            const initialFetchCallsLength = fetch.mock.calls.length
+            jest.runAllTimers()
+            expect(fetch.mock.calls.length - initialFetchCallsLength).toEqual(2)
         })
     })
 })
